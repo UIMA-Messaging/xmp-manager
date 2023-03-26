d740e71581c163b72849feb37636fedea473eea9
greffgreff <greffchandler80@gmail.com>
2023-03-26 14:42:35 +0200

    Cleanups

--- Contracts/User.cs
+++ Contracts/User.cs
@@ -6,8 +6,8 @@
         public string DisplayName { get; set; }
         public string Username { get; set; }
         public string? Image { get; set; }
+        public string EphemeralPassword { get; set; }
         public DateTime? JoinedAt { get; set; }
         public DateTime? EditedAt { get; set; }
-        public string EphemeralPassword { get; set; }
     }
 }
